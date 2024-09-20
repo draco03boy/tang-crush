84ce5795976a9e22833b79b63e64bc08855bb19c
draco03boy <longlvtbc00142@fpt.edu.vn>
2024-09-20 22:14:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bé Nguyên à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Bé Nguyên muốn cho tui tiền đúng hong 😙',
+    desc: 'Bé Nguyên muốn cho tui tiền đúng hong 😙 ',
+    btnYes: 'rất sẵn lòng cho anh Long mượn tiền <33',
+    btnNo: 'Đéo :3',
+    question:'Tại sao lại tự nguyện cho Long tiền <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại gì em là đệ tử của anh Long <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
